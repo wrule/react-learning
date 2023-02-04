2306d99f99733a914495f714b7cbf6ffcbc203d8
jimao <jimao@jimaodeMacBook-Pro.local>
2023-02-04 16:29:52 +0800

    请求接口

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const update = async () => {
+    const rsp = await axios.post('/api/xsea/workspace/page', {
+      pageNum: 1,
+      pageSize: 10,
+    });
+    console.log(rsp);
+  };
+
+  useEffect(() => {
+    update();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
