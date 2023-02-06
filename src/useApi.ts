561b384bcea3528fdb4601833967de283df1c3bc
jimao <jimao@jimaodeMacBook-Pro.local>
2023-02-06 18:27:05 +0800

    尝试性工作

--- src/useApi.ts
+++ src/useApi.ts
@@ -3,6 +3,16 @@ import * as api from './api';
 
 export * from './api';
 
+console.log(Object.entries(api));
+
+interface A {
+  name1: string;
+  name2: string;
+  name3: string;
+}
+
+type B = { [P in keyof A & string as `str_${P}`]: string };
+
 export
 const useList = (params: any, options?: any) => {
   return useJimao(api.list, params, options);
