4f58d24d0868f3faaeb88bbf02fb79dd38588bbe
jimao <jimao@jimaodeMacBook-Pro.local>
2023-02-07 15:03:58 +0800

    提交代码

--- src/useApi.ts
+++ src/useApi.ts
@@ -13,20 +13,29 @@ type B = {
 
 const b: B = { } as any;
 
+
+type Options = any;
+type Params = Parameters<typeof api.list>;
+type ParamsExt = [...Params, Options?];
+
 export
-function useApi_list(...params: Parameters<typeof api.list>) {
-  return useApi(api.list, params);
+function useApi_list(...paramsExt: ParamsExt) {
+  const params = paramsExt.slice(0, paramsExt.length - 1) as Params;
+  const options = paramsExt[paramsExt.length - 1] as Options;
+  return useApi(api.list, params, options);
 };
 
 export
 function useApi<T extends (...args: any) => any>(
   func: T,
   params: Parameters<T>,
+  options?: Options,
 ) {
   return useQuery(
     [func, ...params],
     ({ queryKey }) => {
       return func(queryKey[1]);
     },
+    options,
   );
 }
