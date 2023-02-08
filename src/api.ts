570cc06b4fa1d69d6e7c14712d69113a2635167f
jimao <jimao@jimaodeMacBook-Pro.local>
2023-02-08 13:55:02 +0800

    提交代码

--- src/api.ts
+++ src/api.ts
@@ -2,4 +2,8 @@ import axios from 'axios';
 
 export
 const list = (params: any) =>
-  axios.post('/api/xsea/workspace/list', params).then((rsp) => rsp.data.object as { list: any[], total: number, pageSize: number });
+  axios.post('/api/xsea/workspace/list', params).then((rsp) => rsp.data.object as {
+    list: any[],
+    total: number,
+    pageSize: number,
+  });
