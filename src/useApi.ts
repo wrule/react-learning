fa8d8f3518b51b3206164a76404ce1b841698ece
jimao <jimao@jimaodeMacBook-Pro.local>
2023-02-08 11:35:07 +0800

    提交代码

--- src/useApi.ts
+++ src/useApi.ts
@@ -18,8 +18,8 @@ function useApi<T extends (...args: any) => any>(
   let options: any = undefined;
   let params: any[] = paramsExt;
   if (params.length > func.length) {
-    params = params.slice(0, params.length - 1);
     options = params[params.length - 1];
+    params = params.slice(0, params.length - 1);
   }
   return useQuery(
     [func, ...params],
