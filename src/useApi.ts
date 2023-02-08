3f3020951d57d64e031b07b76af6da8a16f3a4f4
jimao <jimao@jimaodeMacBook-Pro.local>
2023-02-08 11:56:12 +0800

    提交代码

--- src/useApi.ts
+++ src/useApi.ts
@@ -14,23 +14,20 @@ function UseApi<T extends AnyFunction>(api: T) {
   return (...args: UseApiParamsType<T>) => useApi(api, ...args);
 }
 
-const k1 = useQuery([add], ({ queryKey }) => add(1, '2'));
+const k1 = useQuery([add, 1, '2'], ({ queryKey }) => add(1, '2'));
 const k2 = useApi(add, 1, '2');
 
 export
-function useApi<T extends (...args: any) => any>(
-  func: T,
-  ...paramsExt: UseApiParamsType<T>
-) {
+function useApi<T extends AnyFunction>(api: T, ...params: UseApiParamsType<T>) {
   let options: any = undefined;
-  let params: any[] = paramsExt;
-  if (params.length > func.length) {
-    options = params[params.length - 1];
-    params = params.slice(0, params.length - 1);
+  let param_keys: any[] = params;
+  if (param_keys.length > api.length) {
+    options = param_keys[param_keys.length - 1];
+    param_keys = param_keys.slice(0, param_keys.length - 1);
   }
   return useQuery(
-    [func, ...params],
-    ({ queryKey }) => func(...queryKey.slice(1)),
+    [api, ...param_keys],
+    ({ queryKey }) => api(...queryKey.slice(1)) as ReturnType<T>,
     options,
   );
 }
