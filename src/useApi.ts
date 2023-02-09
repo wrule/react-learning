d72a2548b3ec2875beafefa7bb5e86f3cb2b5174
jimao <jimao@jimaodeMacBook-Pro.local>
2023-02-09 15:28:35 +0800

    提交代码

--- src/useApi.ts
+++ src/useApi.ts
@@ -4,16 +4,10 @@ type Options = Parameters<typeof useQuery>[2];
 type AnyFunction = (...args: any) => any | Promise<any>;
 type UseApiParamsType<T extends AnyFunction> = Parameters<T> | [...Parameters<T>, Options?];
 type UseApiReturnType<T extends AnyFunction> = ReturnType<typeof useApi<T>>;
-
-// function add(a: number, b: string) {
-//   return a + b;
-// }
-
-// const k1 = useQuery([add, 1, '2'], ({ queryKey }) => add(1, '2'));
-// const k2 = useApi(add)(1, '2');
+type AwaitedFunction<T extends AnyFunction> = (...args: Parameters<T>) => Awaited<ReturnType<T>>
 
 function _useApi<T extends AnyFunction>(
-  api: T,
+  api: AwaitedFunction<T>,
   ...args: UseApiParamsType<T>
 ) {
   let options: any = undefined;
@@ -24,7 +18,7 @@ function _useApi<T extends AnyFunction>(
   }
   return useQuery(
     [api, ...arg_keys],
-    ({ queryKey }) => api(...queryKey.slice(1)),
+    ({ queryKey }) => api(...queryKey.slice(1) as Parameters<T>),
     options,
   );
 }
